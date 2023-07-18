391e25bc4cd73c5d04f962856acaf4a2c145bf4c
laluashvili <139061275+laluashvili@users.noreply.github.com>
2023-07-18 03:39:43 -0700

    Add files via upload

---
+++ lessom1.py
@@ -0,0 +1,6 @@
+age = input("how old are you: ")
+current_year=input("what year is it ")
+travel_year=input("what year do you want to travel to")
+age_difference=int(travel_year) - int(current_year)
+future_age=int(age)+age_difference
+print(future_age)
